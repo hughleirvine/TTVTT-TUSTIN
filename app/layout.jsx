aa9b68a9dd3886f2395a6b489398df8285dc9f95
Hugh Le <hughleirvine@gmail.com>
2025-07-23 15:23:23 -0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,7 @@ import { Header } from '../components/header';
 export const metadata = {
     title: {
         template: '%s | Netlify',
-        default: 'Netlify Starter'
+        default: 'Ban TTvTT'
     }
 };
 
