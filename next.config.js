b3a528871ad9eebdd7ecd788a6f3429341a100a1
Hugh Le <hughleirvine@gmail.com>
2025-07-27 12:19:53 -0700

    Update next.config.js for image to display

--- next.config.js
+++ next.config.js
@@ -3,4 +3,18 @@ const nextConfig = {
     reactStrictMode: true
 };
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
 module.exports = nextConfig;
